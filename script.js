dda5bf3a89315c0562ccac3d54a3cedfae51d935
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-10-12 15:48:10 +0200

    just-eat-clone.improvements

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let menuItems = [
-    { menu: 'Pizza', price: 9.99, amount: 2 },
-    { menu: 'Pasta', price: 8.99, amount: 5 },
-    { menu: 'Tiramisu', price: 6, amount: 2 }
-  ];
+    { menu: 'Rindfleisch Momos(8 Stück)', description: 'Handgemachte Tibetische Teigtaschen mit 100% Schweizer Rindfleisch - Füllung nach eigenem Geheimrezept', price: 22.50, amount: 1 },
+    { menu: 'Poulet Momos(8 Stück)', description: 'Handgemachte Tibetische Teigtaschen mit einer Füllung aus Schweizer Poulet, Lauch und Erbsen nach eigenem Geheimrezept', price: 22.50, amount: 1 },
+    { menu: 'Vegi Momos(8 Stück)', description: 'Handgemachte Tibetische Teigtaschen mit saisonaler Gemüse - Paneer - Füllung nach eigenem Geheimrezept', price: 22.50, amount: 1 },
+    { menu: 'Vegan Momos(8 Stück)', description: 'Handgemachte Tibetische Teigtaschen mit saisonaler Gemüse - Füllung nach eigenem Geheimrezept', price: 22.50, amount: 1 },
+    { menu: 'Mochis(5 Stück)', description: 'Ein japanischer Reiskuchen, der aus gekochtem Mochi-Gome, eine süßliche Reissorte hergestellt wird.', price: 15.00, amount: 1 },
+    { menu: 'Coca-Cola 1,5l', description: '', price: 7.00, amount: 1 }
+];
+const SHIPPING = 5.90;
+
+
+
+// if amount > 0 add d-none(+) img render amount else remove d-none(+)
+function renderMenu() {
+
+}
+
+
+function renderBasket() {
+
+}
 
 
 function getValueFromInput(inputId) {
@@ -26,7 +42,8 @@ function onAddMenu() {
     let menu = getMenuFromInput();
     let price = getPriceFromInput();
     let index = getMenuIndex(menu);
-    if (index !== -1) {
+
+    if (index !== "not in the json") {
       menuItems[index].amount += 1;
     } else {
       menuItems.push({ menu, price, amount: 1 });
@@ -40,53 +57,15 @@ function getMenuIndex(menu) {
         return i;
       }
     }
-    return -1;
+    return "not in the json";
 }
 
 
+function save() {
 
-let menus = ['Pizza', 'Pasta','Tiramisu'];
-let prices = [9.99, 8.99, 6];
-let amounts = [2, 5, 2];
-
-
-function getValueFromInput(inputId) {
-  let input = document.getElementById(inputId);
-    return input.value.trim();
 }
 
 
-function getMenuFromInput() {
-  let menuInput = getValueFromInput('menu');
-    return menuInput
-}
+function load() {
 
-
-function getPriceFromInput() {
-  let priceInput = getValueFromInput('price');
-    return +priceInput;
-}
-
-
-function onAddMenu() {
-  let menu = getMenuFromInput();
-  let price = getPriceFromInput();
-  let index = getMenuIndex(menu);
-  if(index !== -1) {
-    amounts[index] += 1;
-  } else {
-    menus.push(menu);
-    prices.push(price);
-    amounts.push(1);
-  }
-}
-
-
-function getMenuIndex(menu) {
-  let index = menus.indexOf(menu);
-  if (index >= 0) {
-    return index;
-  } else {
-    return -1;
-  }
 }
